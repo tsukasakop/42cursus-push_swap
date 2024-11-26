c3a9c88182935fbe8ea99cd51f8aa562a7b89129
Tsukasa Kondo <tsukasakop@MacBook-Air-2018.local>
2024-11-27 02:55:51 +0900

    🚧 WIP

--- push_swap.c
+++ push_swap.c
@@ -2,8 +2,10 @@
 #include <stddef.h>
 #include <unistd.h>
 #include <stdio.h>
+#include <stdbool.h>
 #include <stdlib.h>
 #include <ctype.h>
+#include <sys/errno.h>
 
 void	raise_err(char *msg)
 {
@@ -30,11 +32,14 @@ void	assert_integer(char *s, void *to_del, void (*del)(void *))
 		s++;
 	//TODO: Replace to ft_isdigit
 	if(!isdigit(*s))
-		raise_err("Error: invalid number provided.");
+		raise_err("Error: no number provided.");
 	while(isdigit(*s))
 		s++;
 	if (*s)
+	{
+		printf("s=%s ", s);
 		raise_err("Error: invalid number provided.");
+	}
 }
 
 t_stack *str2stack(char *s)
@@ -45,6 +50,19 @@ t_stack *str2stack(char *s)
 		return NULL;
 	// TODO: Replace to ft_atoi
 	p->val = atoi((const char *)s);
+	p->prev = NULL;
+	p->next = NULL;
+	return p;
+}
+
+t_stack *int2stack(int i)
+{
+	t_stack *p;
+	p = malloc(sizeof(t_stack));
+	if(!p)
+		return NULL;
+	// TODO: Replace to ft_atoi
+	p->val = i;
 	p->prev = NULL;
 	p->next = NULL;
 	return p;
@@ -84,6 +102,37 @@ void	del_status(void *ptr)
 		del_status(p->ch[i++]);
 	free(p);
 	return;
+}
+
+void	print_state(t_state *s)
+{
+	printf(".score = %d,\n", s->score);
+	t_stack *i;
+	i = s->a;
+	printf(".a(%p) = ", i);
+	if (i)
+	{
+		printf("%d, ", i->val);
+		i = i->next;
+		while(i != s->a)
+		{
+			printf("%d, ", i->val);
+			i = i->next;
+		}
+	};
+	i = s->b;
+	printf("\n.b(%p) = ", i);
+	if (i)
+	{
+		printf("%d, ", i->val);
+		i = i->next;
+		while(i != s->b)
+		{
+			printf("%d, ", i->val);
+			i = i->next;
+		}
+	}
+	printf("\n");
 }
 
 t_state *arg2state(int n, char **s)
@@ -127,6 +176,226 @@ void validate_arg(int c, char** v)
 	return;
 }
 
+t_stack *dup_stack(t_stack *s)
+{
+	t_stack *p;
+	t_stack *cur_orig;
+	t_stack *cur_dup;
+
+	if(!s)
+		return NULL;
+	p = int2stack(s->val);
+	if(!p)
+		return NULL;
+	cur_orig = s;
+	cur_dup = p;
+	while(cur_orig->next != s)
+	{
+		cur_dup->next = int2stack(cur_orig->next->val);
+		if (!cur_dup->next)
+		{
+			del_stack(p);
+			return NULL;
+		}
+		cur_dup->next->prev = cur_dup;
+		cur_dup = cur_dup->next;
+		cur_orig = cur_orig->next;
+	}
+	cur_dup->next = p;
+	p->prev = cur_dup;
+	return p;
+}
+
+t_state *dup_state(t_state *s)
+{
+	t_state *p;
+	p = (t_state *)calloc(sizeof(t_state), 1);
+	if (!p)
+		return NULL;
+	p->a = dup_stack(s->a);
+	p->b = dup_stack(s->b);
+	if ((s->a && !p->a) || (s->b && !p->b))
+	{
+		free(p->a);
+		free(p->b);
+		free(p);
+		return NULL;
+	}
+	return p;
+}
+
+void	swap(t_stack **s)
+{
+	t_stack *cur;
+	t_stack *last;
+
+	if (!*s || !(*s)->next)
+		return;
+	cur = *s;
+	last = (*s)->prev;
+
+	cur->prev = cur->next;
+	cur->next = cur->prev->next;
+	cur->next->prev = cur;
+	cur = cur->prev;
+	cur->next = cur->prev;
+	cur->prev = last;
+	last->next = cur;
+	*s = cur;
+}
+
+void	push(t_stack **lhs, t_stack **rhs)
+{
+	t_stack *tmp;
+	if (!*lhs)
+		return;
+	tmp = *lhs;
+	if (*lhs == (*lhs)->next)
+		*lhs = NULL;	
+	else
+	{
+		*lhs = tmp->next;
+		tmp->next->prev = tmp->prev;
+		tmp->prev->next = tmp->next;
+	}
+	if(!*rhs)
+	{
+		*rhs = tmp;
+		tmp->next = tmp;
+		tmp->prev = tmp;
+	}
+	else
+	{
+		tmp->next = *rhs;
+		tmp->prev = (*rhs)->prev;
+		tmp->next->prev = tmp;
+		tmp->prev->next = tmp;
+		*rhs = tmp;
+	}
+}
+
+void	rotate(t_stack **s)
+{
+	if (!*s)
+		return;
+	*s = (*s)->next;
+}
+
+void	r_rotate(t_stack **s)
+{
+	if (!*s)
+		return;
+	*s = (*s)->prev;
+}
+
+void	sa(t_state *s)
+{
+	swap(&s->a);
+}
+
+void	sb(t_state *s)
+{
+	swap(&s->b);
+}
+
+void	ss(t_state *s)
+{
+	sa(s);
+	sb(s);
+}
+
+void	pa(t_state *s)
+{
+	push(&s->a, &s->b);
+}
+
+void	pb(t_state *s)
+{
+	push(&s->b, &s->a);
+}
+
+void	ra(t_state *s)
+{
+	rotate(&s->a);
+}
+
+void	rb(t_state *s)
+{
+	rotate(&s->b);
+}
+
+void	rr(t_state *s)
+{
+	ra(s);
+	rb(s);
+}
+
+void	rra(t_state *s)
+{
+	r_rotate(&s->a);
+}
+
+void	rrb(t_state *s)
+{
+	r_rotate(&s->b);
+}
+
+void	rrr(t_state *s)
+{
+	rra(s);
+	rrb(s);
+}
+
+t_state *get_state(t_state *s, t_action a)
+{
+	t_state *ch;
+	ch = dup_state(s);
+	if (!ch)
+		return NULL;
+	void (*act[N_ACTION])(t_state *);
+	act[SA] = sa;
+	act[SB] = sb;
+	act[SS] = ss;
+	act[PA] = pa;
+	act[PB] = pb;
+	act[RA] = ra;
+	act[RB] = rb;
+	act[RR] = rr;
+	act[RRA] = rra;
+	act[RRB] = rrb;
+	act[RRR] = rrr;
+	act[a](s);
+	return ch;
+}
+
+bool	emulate_action(t_state *s, int gen)
+{
+	t_action	i;
+
+	//printf("gen: %d\n", gen);
+	if (gen < 1)
+		return true;
+	i = SA;
+	while(i < N_ACTION)
+	{
+		if(!s->ch[i])
+		{
+			s->ch[i] = get_state(s, i);
+			if (!s->ch[i])
+				return false;
+		}
+		i++;
+	}
+	i = SA;
+	while(i < N_ACTION)
+	{
+		if (!emulate_action(s->ch[i], gen-1))
+			return false;
+		i++;
+	}
+	return true;
+}
+
 void del_game(void *ptr)
 {
 	if(!ptr)
@@ -137,12 +406,55 @@ void del_game(void *ptr)
 	free(p);
 }
 
+void	run_game(t_game *g, int gen)
+{
+	if (!emulate_action(g->cur, gen))
+	{
+		del_game(g);
+		raise_err("Error: emulate failed");
+	}
+}
+
 int main(int argc, char** argv)
 {
 	t_game *g;
 
+	printf("argc=%d\n", argc);
 	validate_arg(argc-1, argv+1);
 	g = init_game(argc-1, argv+1);
-	//game_start(g, 3);
+	run_game(g, 3);
 	del_game(g);
 }
+
+void	test_state(t_game *g)
+{
+	print_state(g->cur);
+	pa(g->cur);
+	print_state(g->cur);
+	pa(g->cur);
+	print_state(g->cur);
+	pa(g->cur);
+	print_state(g->cur);
+	pa(g->cur);
+	print_state(g->cur);
+	pb(g->cur);
+	print_state(g->cur);
+	sa(g->cur);
+	print_state(g->cur);
+	sb(g->cur);
+	print_state(g->cur);
+	ss(g->cur);
+	print_state(g->cur);
+	ra(g->cur);
+	print_state(g->cur);
+	rb(g->cur);
+	print_state(g->cur);
+	rr(g->cur);
+	print_state(g->cur);
+	rra(g->cur);
+	print_state(g->cur);
+	rrb(g->cur);
+	print_state(g->cur);
+	rrr(g->cur);
+	print_state(g->cur);
+}	
--- push_swap.h
+++ push_swap.h
@@ -1,3 +1,22 @@
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+
+#define N_ACTION 11
+
+typedef enum e_action
+{
+	SA,
+	SB,
+	SS,
+	PA,
+	PB,
+	RA,
+	RB,
+	RR,
+	RRA,
+	RRB,
+	RRR
+}	t_action;
 
 typedef struct s_stack
 {
@@ -5,39 +24,41 @@ typedef struct s_stack
 	struct s_stack *prev;
 	struct s_stack *next;
 }	t_stack;
-#define N_ACTION 11
+
 typedef struct s_state
 {
 	int	score;
 	t_stack	*a;
 	t_stack	*b;
-	struct s_stack *ch[N_ACTION];
+	struct s_state *ch[N_ACTION];
 }	t_state;
-typedef struct s_set
+
+typedef struct
 {
 }	t_set;
+
 typedef struct
 {
 	t_state *cur;
 	t_set *set;
 }	t_game;
 
-void	swap(t_stack *s);
-void	push(t_stack *lhs, t_stack *rhs);
-void	rotate(t_stack *s);
-void	r_rotate(t_stack *s);
+void	swap(t_stack **s);
+void	push(t_stack **lhs, t_stack **rhs);
+void	rotate(t_stack **s);
+void	r_rotate(t_stack **s);
 
-void	sa(t_state s);
-void	sb(t_state s);
-void	ss(t_state s);
-void	pa(t_state s);
-void	pb(t_state s);
-void	ra(t_state s);
-void	rb(t_state s);
-void	rr(t_state s);
-void	rra(t_state s);
-void	rrb(t_state s);
-void	rrr(t_state s);
+void	sa(t_state *s);
+void	sb(t_state *s);
+void	ss(t_state *s);
+void	pa(t_state *s);
+void	pb(t_state *s);
+void	ra(t_state *s);
+void	rb(t_state *s);
+void	rr(t_state *s);
+void	rra(t_state *s);
+void	rrb(t_state *s);
+void	rrr(t_state *s);
 
 t_state* get_given_state(int argc, char** argv);
 t_state* dup_state(t_state* s);
@@ -49,3 +70,4 @@ t_game* init_game(int c, char **v);
 void	run_game(t_game*, int gen);
 
 void	validate_arg(int, char**);
+#endif
